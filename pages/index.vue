da78148a396b8554c82eea5ea785ed7fd11f2536
Methmal1001 <hau.methmal1@gmail.com>
2026-04-21 11:42:27 +0530

    Initial commit - Nuxt 3 frontend

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-yellow-500 font-bold">Welcome to Region Selector App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
